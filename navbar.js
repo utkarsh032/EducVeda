30cbfbc60b80c474a7d5fe24532b67a58c8b1cc6
utkarsh032 <utkarshraj525@gmail.com>
2024-11-10 16:58:15 +0530

    Fixxed UI

--- navbar.js
+++ navbar.js
@@ -1,12 +1,16 @@
 export default function loadNavbar() {
   document.querySelector('nav').innerHTML = `
     <div class="logo">
-      <!-- <img src="logo.png" alt="Learnific Logo" /> -->
-      <span>EduFuse</span>
+      <!-- <img src="logo.png" alt="EduVeda Logo" /> -->
+      <h1>Educ<span>Veda.</span></h1>
     </div>
     <ul class="nav-links">
       <li><a href="index.html">Home</a></li>
       <li><a href="create.html">Create Course</a></li>
+      <li><a href="favourites.html">My Favourite Courses</a></li>
+      <li><a href="user.html">User</a></li>
+      <li><a href="chat.html">Chat</a></li>
+      <li><a href="support.html">Support</a></li>
       <li><a href="#cart">Cart</a></li>
     </ul>
     <div class="hamburger" onclick="toggleMenu()">
@@ -14,8 +18,9 @@ export default function loadNavbar() {
       <span></span>
       <span></span>
     </div>
-  `
+  `;
 }
+
 export function toggleMenu() {
   const navbarLinks = document.querySelector('.nav-links');
   navbarLinks.classList.toggle('open');
